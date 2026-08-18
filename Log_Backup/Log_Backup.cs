0027a44b9dee41a204a01af2ff51fe91bc6d4ca1
CoolLKK PS <PSKyle5926@outlook.com>
2026-08-19 06:14:02 +0800

    Add project files.

---
+++ Log_Backup/Log_Backup.cs
@@ -0,0 +1,25 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using UnityEngine;
+
+namespace Log_Backup
+{
+    [BepInPlugin(PLUGIN_GUID, PLUGIN_NAME, PLUGIN_VERSION)]
+    public class Log_BackupPlugin : BaseUnityPlugin
+    {
+        private void Awake()
+        {
+            Instance = this;
+            logger = base.Logger;
+            gameObject.hideFlags = HideFlags.HideAndDontSave;
+        }
+
+        public const string PLUGIN_GUID = "Log_Backup";
+        public const string PLUGIN_NAME = "Log_Backup";
+        public const string PLUGIN_VERSION = "1.0.0";
+        public const string PLUGIN_VERSION_FULL = PLUGIN_VERSION + ".0";
+
+        public static ManualLogSource logger;
+        public static Log_BackupPlugin Instance;
+    }
+}
---
+++ Log_Backup/Properties/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+﻿using Log_Backup;
+using System.Reflection;
+
+[assembly: AssemblyVersion(Log_BackupPlugin.PLUGIN_VERSION_FULL)]
+[assembly: AssemblyCompany(Log_BackupPlugin.PLUGIN_NAME)]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyFileVersion(Log_BackupPlugin.PLUGIN_VERSION_FULL)]
+[assembly: AssemblyInformationalVersion(Log_BackupPlugin.PLUGIN_VERSION)]
+[assembly: AssemblyProduct(Log_BackupPlugin.PLUGIN_NAME)]
+[assembly: AssemblyTitle(Log_BackupPlugin.PLUGIN_NAME)]
